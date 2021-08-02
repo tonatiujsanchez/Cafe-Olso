d912b447bbf7840848163566bf08e062551a71d0
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2021-08-02 03:27:47 -0500

    Animaciones agregadas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 const nav = document.querySelector('#nav');
 const btnNav = document.querySelector('#btn-nav');
 
-
 btnNav.addEventListener( 'click', ()=>{
     nav.classList.toggle('toggle-nav');
+});
+
+
+
+
+window.addEventListener( 'scroll', ()=>{
+
+    const animationFade = ( speed )=>{
+        const elements = document.querySelectorAll(`.animation-${speed}`);
+    
+        elements.forEach(element => {        
+            const screenHeight = window.innerHeight;
+            const distancia = element.getBoundingClientRect().top;
+            // element.classList.add('traslation');
+        
+            if( distancia <= screenHeight ){
+                element.classList.add(`traslation-${speed}`);
+            }
+        });
+        
+    }
+
+    animationFade('fast');
+    animationFade('medium');
+    animationFade('slow');
 });
